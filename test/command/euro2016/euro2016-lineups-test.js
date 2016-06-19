dda600a139479fb4c2437090f2628dd9b9dabd8a
Gary McAdam <gary@mcad.am>
2016-06-19 19:07:34 +0100

    remove unnecessary call

--- test/command/euro2016/euro2016-lineups-test.js
+++ test/command/euro2016/euro2016-lineups-test.js
@@ -34,66 +34,59 @@ describe('Euro2016!lineups', () => {
                 done();
             });
     });
-    describe('match but no lineups', () => {
-        it('should show no lineups for match if none', done => {
-            const clock = sinon.useFakeTimers(new Date(2016,5,19,10,0,0).getTime());
-            nock('http://daaseuro2016.uefa.com/')
-                .filteringPath(() => {
-                    return '/';
-                })
-                .get('/')
-                .query(() => {
-                    return true;
-                })
-                .replyWithFile(200, __dirname + '/stub/euro2016-lineup-competitions.json')
-                .filteringPath(() => {
-                    return '/';
-                })
-                .get('/')
-                .query(() => {
-                    return true;
-                })
-                .replyWithFile(200, __dirname + '/stub/euro2016-lineup-empty.json');
-            const command = { from: 'John', to: '#channel', text: 'next', args: ['next'] };
-            const client = { say: sinon.spy() };
-            lineups(command, client)
-                .finally(() => {
-                    assert.isTrue(client.say.called);
-                    expect(client.say.getCall(0).args[1]).to.contain('No lineups released for');
-                    clock.restore();
-                    done();
-                });
-        });
+
+    it('should show no lineups for match if none', done => {
+        const clock = sinon.useFakeTimers(new Date(2016,5,19,10,0,0).getTime());
+        nock('http://daaseuro2016.uefa.com/')
+            .filteringPath(() => {
+                return '/';
+            })
+            .get('/')
+            .query(() => {
+                return true;
+            })
+            .replyWithFile(200, __dirname + '/stub/euro2016-lineup-competitions.json')
+            .filteringPath(() => {
+                return '/';
+            })
+            .get('/')
+            .query(() => {
+                return true;
+            })
+            .replyWithFile(200, __dirname + '/stub/euro2016-lineup-empty.json');
+        const command = { from: 'John', to: '#channel', text: 'next', args: ['next'] };
+        const client = { say: sinon.spy() };
+        lineups(command, client)
+            .finally(() => {
+                assert.isTrue(client.say.called);
+                expect(client.say.getCall(0).args[1]).to.contain('No lineups released for');
+                clock.restore();
+                done();
+            });
     });
-    describe('no matches and no lineups', () => {
-        it('should show no lineups if none', done => {
-            const clock = sinon.useFakeTimers(new Date(2016,8,18,16,0,0).getTime());
-            nock('http://daaseuro2016.uefa.com/')
-                .filteringPath(() => {
-                    return '/';
-                })
-                .get('/')
-                .query(() => {
-                    return true;
-                })
-                .replyWithFile(200, __dirname + '/stub/euro2016-lineup-competitions.json')
-                .filteringPath(() => {
-                    return '/';
-                })
-                .get('/')
-                .query(() => {
-                    return true;
-                })
-                .replyWithFile(200, __dirname + '/stub/euro2016-lineup.json');
-            const command = { from: 'John', to: '#channel', text: '', args: [] };
-            const client = { say: sinon.spy() };
-            lineups(command, client)
-                .finally(() => {
-                    assert.isTrue(client.say.called);
-                    expect(client.say.getCall(0).args[1]).to.contain('No lineups');
-                    clock.restore();
-                    done();
-                });
-        });
+
+    it('should show no lineups if none', done => {
+        const clock = sinon.useFakeTimers(new Date(2016,8,18,16,0,0).getTime());
+        nock('http://daaseuro2016.uefa.com/')
+            .filteringPath(() => {
+                return '/';
+            })
+            .get('/')
+            .query(() => {
+                return true;
+            })
+            .replyWithFile(200, __dirname + '/stub/euro2016-lineup-competitions.json')
+            .filteringPath(() => {
+                return '/';
+            });
+        const command = { from: 'John', to: '#channel', text: '', args: [] };
+        const client = { say: sinon.spy() };
+        lineups(command, client)
+            .finally(() => {
+                assert.isTrue(client.say.called);
+                expect(client.say.getCall(0).args[1]).to.contain('No lineups');
+                clock.restore();
+                done();
+            });
     });
 });
